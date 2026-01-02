c4fea7dca71070398922d86deb26f9e047811dab
DenisZekiria <45890858+DenisZekiria@users.noreply.github.com>
2026-01-02 12:06:58 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'fitness-v1';
+const urlsToCache = [
+  './fitness-02-01-2025.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
